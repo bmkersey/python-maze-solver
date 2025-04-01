d9a14429326a514fb61d88f9caa3c833a382b218
Brendan <bmkersey@gmail.com>
2025-04-01 13:43:22 -0400

    added Line and Point classes. Updated Window and main

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
 from Window import Window
+from Point import Point
+from Line import Line
 
 win = Window(800,600)
+start_point = Point(20,20)
+end_point = Point(600,20)
+full_line = Line(start_point, end_point)
+win.draw_line(full_line, "black")
 win.wait_for_close()
